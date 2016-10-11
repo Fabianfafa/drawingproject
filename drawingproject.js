e1ffde65b92df32be3fe14f86bfffb002eaf7af0
Fabian Ochoa Eduardo  Saldana <Fabianochoa753@gmail.com>
2016-10-10 23:58:48 -0700

    20 shaps css  background
    circles in place of shapes and i havve 20  plus shapes

--- drawingproject.js
+++ drawingproject.js
@@ -31,8 +31,134 @@ canvas.fillStyle = "red";
 canvas.fill();
 canvas.fillRect(409,264,900,372);
 
-//circle
+//circle 1
 canvas.beginPath();
 canvas.fillStyle = "white";
-canvas.arc(68,180,50,0,2*Math.PI,true);
+canvas.arc(40,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle2
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(110,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle3
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(180,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle4
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(250,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle5
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(320,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle6
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(390,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle7
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(460,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle8
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(530,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle9
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(600,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle10
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(670,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle11
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(740,180,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle 12
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(40,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle13
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(110,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle14
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(180,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle15
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(250,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle16
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(320,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle17
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(390,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle18
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(460,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle19
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(530,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle20
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(600,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle20
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(670,720,30,0,2*Math.PI,true);
+canvas.fill();
+
+//circle21
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(740,720,30,0,2*Math.PI,true);
 canvas.fill();
